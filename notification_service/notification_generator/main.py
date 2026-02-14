7e3c2e8004f34b484f0853920fd1aface8e1aa67
eabuntov <e.a.buntov@urfu.ru>
2026-02-14 15:57:56 +0500

    WIP: taskiq

--- notification_service/notification_generator/main.py
+++ notification_service/notification_generator/main.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from taskiq import TaskiqScheduler
 from taskiq.schedule_sources import LabelScheduleSource
 
@@ -9,5 +11,13 @@ scheduler = TaskiqScheduler(
     sources=[LabelScheduleSource(broker)],
 )
 
+async def run():
+    await broker.startup()
+    await scheduler.startup()
+    while True:
+        task = await tasks.generate_one_time_notification.kiq(1)
+        await task.wait_result(timeout=2)
+        await asyncio.sleep(1)
+
 if __name__ == "__main__":
-    scheduler.run()
+    asyncio.run(run())
