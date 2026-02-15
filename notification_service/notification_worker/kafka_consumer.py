fd2e417661e71e361087813610d4002dd0044954
eabuntov <e.a.buntov@gmail.com>
2026-02-15 17:20:10 +0500

    KAFKA_BOOTSTRAP to environment

--- notification_service/notification_worker/kafka_consumer.py
+++ notification_service/notification_worker/kafka_consumer.py
@@ -1,10 +1,15 @@
 import json
+import os
+
 from kafka import KafkaConsumer
+
+
+KAFKA_BOOTSTRAP = os.getenv("KAFKA_BOOTSTRAP")
 
 consumer = KafkaConsumer(
     "notification.events.high",
     "notification.events.bulk",
-    bootstrap_servers="kafka:9092",
+    bootstrap_servers=KAFKA_BOOTSTRAP,
     group_id="notification-workers",
     enable_auto_commit=False,
     value_deserializer=lambda v: json.loads(v.decode("utf-8")),
